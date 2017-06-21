69f1c740d29312bbd6298302d0c2732cd4760845
revyh <revyh@mail.ru>
2017-06-22 02:19:33 +0700

    add media file

--- toolsUtils/build/misc.js
+++ toolsUtils/build/misc.js
@@ -3,8 +3,9 @@
 const {resolve} = require('path');
 const merge = require('webpack-merge');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
-function misc(config, {absSrc}) {
+function misc(config, {absSrc, absDist}) {
   const miscDir = resolve(absSrc, 'misc');
   const htmlEntry = resolve(absSrc, 'index.html');
 
@@ -36,6 +37,12 @@ function misc(config, {absSrc}) {
         inject: false,
         template: htmlEntry,
       }),
+      new CopyWebpackPlugin([
+        {
+          from: resolve(miscDir, 'media.json'),
+          to: resolve(absDist, 'media.json'),
+        },
+      ]),
     ],
   });
 }
